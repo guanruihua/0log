6850cb34b86e7af298c1693eb350ea6e19546690
grh-gitee <3494242908@qq.com>
2023-12-06 17:36:42 +0800

    feat: test

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 > - `console.log`  Secondary packaging  
 > - 不支持占位符语法  
 > - log 填充类型
+
+[官方console标准](https://console.spec.whatwg.org/)
--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -2,6 +2,6 @@ import { __0__log__config__ } from '..'
 
 __0__log__config__.DATE = true
 
-// import './log'
-// import './table'
+import './log'
+import './table'
 import './group'
