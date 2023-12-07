1e815d59be221d151e9973122d27f109a9651464
grh-gitee <3494242908@qq.com>
2023-12-07 16:51:06 +0800

    feat: dev

--- src/log.ts
+++ src/log.ts
@@ -6,12 +6,6 @@ import { color } from 'rh-color'
 
 function logParams(list: unknown[]) {
 	const result: unknown[] = []
-
-	// if (isBrowser) {
-	// 	if (list.length === 1) {
-	// 		return [getTypeStr(list[0]), list[0]]
-	// 	}
-	// }
 	list.forEach(item => {
 		result.push(item)
 		result.push(getTypeStr(item))
@@ -23,24 +17,27 @@ export function log(...rest: unknown[]) {
 	if (isBrowser) {
 		const len = rest.length
 		const formatItem = (value: unknown, depth: number = 0) => {
+			
 			if (isString(value)) {
-				const res = color(stringify(value, null, 2), 'Cyan') + color(` <${type(value)}>`, 'Green', 'Italic')
+				const res = color(stringify(value, null, 2), 'Cyan', 'Bright') + color(` <${type(value)}>`, 'Green', 'Italic')
 				if (depth === 0) {
 					return res
 				}
 				return res.replace(/^/gm, new Array(depth).fill('  ').join(''))
 			}
+
 			if (isNumber(value)) {
-				const res = color(stringify(value, null, 2), 'Magenta') + color(` <${type(value)}>`, 'Green', 'Italic')
+				const res = color(stringify(value, null, 2), 'Bright') + color(` <${type(value)}>`, 'Green', 'Italic')
 				if (depth === 0) {
 					return res
 				}
 				return res.replace(/^/gm, new Array(depth).fill('  ').join(''))
 			}
+
 			if (isObject(value)) {
 				let resList = ['{']
 				for (const key in value) {
-					resList.push(`  ${color(key, 'Yellow')}: ${formatItem(value[key])}`)
+					resList.push(`  ${color(key, 'Blue', 'Bright')}: ${formatItem(value[key])}`)
 				}
 				resList.push('}')
 				const res = resList.join('\n')
@@ -49,24 +46,39 @@ export function log(...rest: unknown[]) {
 				}
 				return res.replace(/^/gm, new Array(depth).fill('  ').join(''))
 			}
+
 			if (isArray(value)) {
-				return `[\n ${value.map(i => formatItem(i, depth + 1)).join(',\n')}\n]` + color(` <${type(value)}:${value.length}>`, 'Green', 'Italic')
+				return `[\n ${value.map(i => formatItem(i, depth + 1)).join(',\n')}\n]`
 			}
-			return stringify(value, null, 2) + color(` <${type(value)}>`, 'Green', 'Italic')
+
+			const getResValue = () => {
+				let val = stringify(value, null, 2)
+				if (val[0] === '{' && val[val.length - 1] === '}') {
+					try {
+						const obj = JSON.parse(JSON.stringify(value))
+						if (isObject(obj)) {
+							val = formatItem(obj)
+						}
+
+					} catch (error) {
+						// eslint-disable-line
+					}
+				}
+				return val.replaceAll('\\"', '')
+
+			}
+			const resValue = getResValue()
+
+			return resValue + color(` <${type(value)}>`, 'Green', 'Italic')
 		}
 		const getFormatStr = () => {
 			if (len === 0) {
 				const val = rest[0]
 				if (isString(val)) {
-					return color(stringify(val, null, 2), 'Cyan') + color(` <${type(val)}>`, 'Green', 'Italic')
+					return color(stringify(val, null, 2), 'Cyan', 'Bright') + color(` <${type(val)}>`, 'Green', 'Italic')
 				}
 			}
 			return rest.map(i => formatItem(i)).join('\n')
-			// const str = color(stringify(val, null, 2), 'Cyan') +
-			// 	color(` <${type(val)}>`, 'Green')
-			// const str = (stringify(val.map(i => color(stringify(i), 'Bright')), null, 2), 'Cyan') +
-			// 	color(` <${type(val)}>`, 'Green')
-			// return stringify(rest, null, 2)
 		}
 		console.log(getFormatStr())
 		return;
